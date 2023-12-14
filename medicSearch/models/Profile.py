b517a60eb701c4e3aeb972dc7dd207acdd6008c6
LuanCarvalho <luan_carvalho1997@hotmail.com>
2023-12-14 19:41:04 -0300

    ✨ feat: adding fields to Profile model

--- medicSearch/models/Profile.py
+++ medicSearch/models/Profile.py
@@ -8,6 +8,9 @@ class Profile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     token = models.CharField(max_length=255, null=True, blank=True)
+    favorites = models.ManyToManyField(User, blank=True, related_name='favorites')
+    specialites= models.ManyToManyField(Speciality, blank=True, related_name='specialites')
+    addresses = models.ManyToManyField(Address, blank=True, related_name='addresses')
 
     def __str__(self) -> str:
         return self.user.username
