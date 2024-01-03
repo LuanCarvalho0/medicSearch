714bad5b8032b37812e3fc35b8a16a69813ecd8a
LuanCarvalho <luan_carvalho1997@hotmail.com>
2024-01-02 21:36:14 -0300

    ✨ feat: adding profile view

--- medicSearchAdmin/urls.py
+++ medicSearchAdmin/urls.py
@@ -7,5 +7,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('medicSearch.urls.HomeUrls')),
+    path('profile/', include('medicSearch.urls.ProfileUrls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
