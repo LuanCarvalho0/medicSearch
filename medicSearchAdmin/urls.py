479f02f81c0e993dff87c35166020eec232e0a4b
LuanCarvalho <luan_carvalho1997@hotmail.com>
2024-01-12 20:51:03 -0300

    ✨ feat: implemented user login

--- medicSearchAdmin/urls.py
+++ medicSearchAdmin/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('medicSearch.urls.HomeUrls')),
+    path('', include('medicSearch.urls.AuthUrls')),
     path('profile/', include('medicSearch.urls.ProfileUrls')),
     path('medic/', include('medicSearch.urls.MedicUrls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
